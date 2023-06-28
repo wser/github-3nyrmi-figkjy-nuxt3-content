d9d6a3eb4bc32635882c8edba79b436cee3985f2
wser <w.web.dev@gmail.com>
2023-06-28 13:34:38 +0200

    3 files modified

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,27 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content'],
+  modules: [
+    '@nuxtjs/tailwindcss',
+    'nuxt-icon',
+    '@nuxt/content',
+    '@nuxtjs/apollo'
+  ],
+  runtimeConfig: {
+    githubToken: process.env.GITHUB_TOKEN
+  },
+  content: {
+    // https://content.nuxtjs.org/api/configuration
+    highlight: {
+      theme: 'nord',
+      preload: ['ts','js','css','java','json','bash','vue']
+    }
+  },
+  apollo: {
+    clients: {
+      default: {
+        tokenName: 'github-token',
+        httpEndpoint: 'https://api.github.com/graphql'
+      }
+    }
+  }
 });
