f5b886967637d03340c26d5b3bdc29ced09679b6
wser <w.web.dev@gmail.com>
2023-06-28 13:44:20 +0200

    1 binaries uploaded

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,27 +1,27 @@
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
+// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss',
     'nuxt-icon',
     '@nuxt/content',
-    '@nuxtjs/apollo'
+    '@nuxtjs/apollo',
   ],
   runtimeConfig: {
-    githubToken: process.env.GITHUB_TOKEN
+    githubToken: process.env.GITHUB_TOKEN,
   },
   content: {
     // https://content.nuxtjs.org/api/configuration
     highlight: {
       theme: 'nord',
-      preload: ['ts','js','css','java','json','bash','vue']
-    }
+      preload: ['ts', 'js', 'css', 'java', 'json', 'bash', 'vue'],
+    },
   },
   apollo: {
     clients: {
       default: {
         tokenName: 'github-token',
-        httpEndpoint: 'https://api.github.com/graphql'
-      }
-    }
-  }
+        httpEndpoint: 'https://api.github.com/graphql',
+      },
+    },
+  },
 });
